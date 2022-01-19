e611e1e689aba8bcaa6cae37856b2bcbd8ccbe49
evann delaunay <evann.delaunay@gmail.com>
2022-01-19 13:27:11 +0100

    -update camera processing

--- Assets/Scripts/CameraProcessing.cs
+++ Assets/Scripts/CameraProcessing.cs
@@ -9,10 +9,13 @@ using UnityEngine.UI;
 public class CameraProcessing : MonoBehaviour
 {
     public int cameraIndex = 0;
+    public int resolution = 0;
     public RawImage imgDisplay;
     private VideoCapture capture;
     private Mat camImg;
     private Texture2D camTexture;
+
+    private bool hasFrame = false;
 
     private void Awake()
     {
@@ -23,7 +26,12 @@ public class CameraProcessing : MonoBehaviour
             Debug.Log(device.name);
         }
 
-        capture = new VideoCapture(cameraIndex, VideoCapture.API.Ffmpeg);
+        capture = new VideoCapture(cameraIndex, VideoCapture.API.DShow, new Tuple<CapProp, int>[]
+        {
+            new Tuple<CapProp, int>(CapProp.Fps, 30), 
+            new Tuple<CapProp, int>(CapProp.FrameWidth,1280), 
+            new Tuple<CapProp, int>(CapProp.FrameHeight,720)
+        });
         camImg = new Mat(capture.Height, capture.Width, DepthType.Default, 3);
         capture.ImageGrabbed += CaptureOnImageGrabbed;
         capture.Start();
@@ -33,6 +41,7 @@ public class CameraProcessing : MonoBehaviour
     {
         capture.Retrieve(camImg);
         Debug.Log("Frame");
+        hasFrame = true;
     }
 
     private void OnApplicationQuit()
@@ -50,11 +59,11 @@ public class CameraProcessing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (capture.IsOpened)
-            capture.Grab();
-        /*if (capture.IsOpened)
+        if (capture.IsOpened && hasFrame)
         {
-
-        }*/
+            hasFrame = false;
+            imgDisplay.texture = camImg.ToTexture2D();
+            //capture.Grab();
+        }
     }
 }
